b26f7eaf5e38f0fac9c164a66af8d44cb2a0a742
asleepann <66643655+asleepann@users.noreply.github.com>
2020-11-04 22:50:45 +0300

    Update ex1.c

--- week12/ex1.c
+++ week12/ex1.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-int main(){
+int main() {
     FILE* dev_random = fopen("/dev/random", "r"); // open /dev/random for reading
     char random_chars[20];
     fscanf(dev_random, "%20s", &random_chars);// read random characters from it
     fclose(dev_random);
     FILE* ex1_txt = fopen("ex1.txt", "w"); // open file for writing
-    fprintf(ex1_txt,"%s",random_chars); // write random chars to that file
+    fprintf(ex1_txt, "%s", random_chars); // write random chars to that file
     fclose(ex1_txt);
     return 0;
 }
